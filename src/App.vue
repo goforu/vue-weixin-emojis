7bfb7dd82126ccde64f75daf9ba3283f63e101ac
goforu <99goforu@gmail.com>
2021-04-07 15:44:46 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EmojiPicker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmojiPicker from './components/EmojiPicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmojiPicker
   }
 }
 </script>
---
+++ src/components/EmojiPicker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
